278767b010b37965b182dcebfeffec63e5bdfe6d
GuilhermeMTrindade <guilherme.trindade1600@gmail.com>
2023-07-04 20:47:45 -0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-import 'dart:io';
 
 import 'package:flutter/material.dart';
 
@@ -13,57 +11,72 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Lista de Tarefas',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
         home: Scaffold(
-          appBar: AppBar(
-            title: Text('Tarefas'),
-          ),
-          body: Container(
-                child: Stack(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: BorderRadius.circular(10)),
-                      height: 140,
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                          color: Color.fromARGB(250, 255, 255, 255),
-                          borderRadius: BorderRadius.circular(5)),
-                      height: 100,
-                      child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                        Container(
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(5)),
-                          child: Image.network('https://static-00.iconduck.com/assets.00/flutter-icon-1651x2048-ojswpayr.png'),
-                          height: 100,
-                          width: 75,
-                        ),
-                        Text('Aprender Flutter', style: TextStyle(fontSize: 18)),
-                        ElevatedButton(onPressed: (){}, child: Icon(Icons.arrow_drop_up))
-                      ]),
-                    ),
-                  ],
-                ),
-                color: Color.fromARGB(200, 208, 221, 237),
-          )
+    appBar: AppBar(
+    title: Text('Tarefas'),
+    ),
+    body: Container(
+      color: Color.fromARGB(255, 208,221,237),
+      child: ListView(
+        children: [
+          Task('Aprender Flutter', 'https://static-00.iconduck.com/assets.00/flutter-icon-1651x2048-ojswpayr.png'),
+          Task('Aprender Inglês', 'https://cdn-icons-png.flaticon.com/512/1377/1377975.png'),
+          Task('Academia','https://media.istockphoto.com/id/1212104006/pt/vetorial/gym-icon.jpg?s=612x612&w=0&k=20&c=yAuwXWzYJ46MOF9GSqozjq2f673VEIw0_hTSq03bV9w='),
+          Task('Trabalhos da Faculdade','https://cdn6.aptoide.com/imgs/1/3/2/1328cfccee2d7f932774c96637eb5b1f_fgraphic.png'),
+          Task('Jogar League of Legends','https://bk.ibxk.com.br/2019/07/26/26171514413327.jpg')
+        ],
+      ),
+    ),
     )
     );
   }
 }
 
 class Task extends StatelessWidget {
-  const Task({super.key});
+  final String nome;
+  final String imagem;
+  const Task(this.nome,this.imagem,{Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      // color: Color.fromARGB(255, 208,221,237),
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10)),
+                height: 140,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    borderRadius: BorderRadius.circular(5)),
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5)),
+                        child: Image.network(imagem),
+                        height: 100,
+                        width: 75,
+                      ),
+                      Text(nome, style: TextStyle(fontSize: 18)),
+                      ElevatedButton(onPressed: (){}, child: Icon(Icons.arrow_drop_up)),
+                    ]),
+              ),
+            ],
+          ),
+        ),
+    );
   }
 }
-
